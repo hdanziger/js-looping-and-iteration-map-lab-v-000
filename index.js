2bca333099423aeb8c91d6e14e758f57a6f4bdb9
Hannah Danziger <hdanziger8@gmail.com>
2018-02-14 20:36:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ function nameToAttributes(drivers) {
     });
 }
 
-
+function attributesToPhrase(drivers) {
+    return drivers.map(function(driver) {
+        return `${driver.name} is from ${driver.hometown}`
+    });
+}
